356b66e3c9323050e47b883239d78bbf363568bc
Robin Clarkson <Robin@sfs-wifi-dhcp-10-143-218-109.wifi.sfsu.edu>
2019-10-01 12:15:39 -0700

    Created matrixInit()

--- matrixTokenizerSorting.cpp
+++ matrixTokenizerSorting.cpp
@@ -3,6 +3,15 @@
 #include <string>
 using namespace std;
 
-int main() {
+void matrixInit( vector< vector<int> >& matrix, int numRows, int numCols) {
+    matrix.resize(numRows);
+    for( int i = 0; i < numRows; i++ ) {
+        matrix[i] = vector<int>(numCols);
+    }
+}
 
+int main() {
+    vector< vector<int> > matrix;
+    int numRows, numCols;
+    matrixInit( matrix, numRows, numCols );
 }
